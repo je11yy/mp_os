87e56f9935bf25aba695a2a51581512d7ea83281
je11yy <l4da-vlasov@yandex.ru>
2024-03-22 21:46:29 +0300

    Allocator global heap fixed, logger with guard added

--- allocator/allocator_global_heap/src/allocator_global_heap.cpp
+++ allocator/allocator_global_heap/src/allocator_global_heap.cpp
@@ -7,41 +7,38 @@
 allocator_global_heap::allocator_global_heap(
     logger *logger)
 {
-    if (get_logger()) logger->trace("[Begin] [Allocator Global Heap] Constructor\n");
+    trace_with_guard("[Begin] [Allocator Global Heap] Constructor\n");
     _logger = logger;
-    allocator * alc = this;
-    if (get_logger()) logger->trace("[End] [Allocator Global Heap] Constructor\n");
+    trace_with_guard("[End] [Allocator Global Heap] Constructor\n");
 }
 
 allocator_global_heap::~allocator_global_heap()
 {
-    if (get_logger()) _logger->trace("[Begin] [Allocator Global Heap] Destructor\n");
-    if (get_logger()) _logger->trace("[End] [Allocator Global Heap] Destructor\n");
+    trace_with_guard("[Begin] [Allocator Global Heap] Destructor\n");
+    trace_with_guard("[End] [Allocator Global Heap] Destructor\n");
 }
 
 allocator_global_heap::allocator_global_heap(
     allocator_global_heap &&other) noexcept
 {
-    if (get_logger()) _logger->trace("[Begin] [Allocator Global Heap] Move constructor\n");
+    trace_with_guard("[Begin] [Allocator Global Heap] Move constructor\n");
     _logger = std::exchange(other._logger, nullptr);
-    if (get_logger()) _logger->trace("[End] [Allocator Global Heap] Move constructor\n");
+    trace_with_guard("[End] [Allocator Global Heap] Move constructor\n");
 }
 
 allocator_global_heap &allocator_global_heap::operator=(
     allocator_global_heap &&other) noexcept
 {
-    if (get_logger()) _logger->trace("[Begin] [Allocator Global Heap] Move operator\n");
-    if (this == &other) {
-        return *this;
-    }
+    trace_with_guard("[Begin] [Allocator Global Heap] Move operator\n");
+    if (this == &other) return *this;
     std::swap(_logger, other._logger);
-    if (get_logger()) _logger->trace("[End] [Allocator Global Heap] Move operator\n");
+    trace_with_guard("[End] [Allocator Global Heap] Move operator\n");
     return *this;
 }
 
 [[nodiscard]] void *allocator_global_heap::allocate(size_t value_size, size_t values_count)
 {
-    if (get_logger()) _logger->debug("[Begin] [Allocator Global Heap] Allocate function\n");
+    debug_with_guard("[Begin] [Allocator Global Heap] Allocate function\n");
 
     block_size_t requested_size = value_size * values_count;
     block_size_t meta_size = sizeof(allocator*) + sizeof(size_t);
@@ -51,7 +48,7 @@ allocator_global_heap &allocator_global_heap::operator=(
     block_pointer_t new_block = ::operator new(size);
     if (new_block == nullptr)
     {
-        if (get_logger()) _logger->error("[Allocate function] Can't allocate\n");
+        error_with_guard("[Allocate function] Can't allocate\n");
         throw std::bad_alloc();
     }
 
@@ -63,14 +60,14 @@ allocator_global_heap &allocator_global_heap::operator=(
 
     block_pointer_t result_block = reinterpret_cast<uint8_t*>(new_block) + meta_size;
 
-    if (get_logger()) _logger->debug("[End] [Allocator Global Heap] Allocate function\n");
+    debug_with_guard("[End] [Allocator Global Heap] Allocate function\n");
 
     return result_block;
 }
 
 void allocator_global_heap::deallocate(void *at)
 {
-    if (get_logger()) _logger->debug("[Begin] [Allocator Global Heap] Deallocate function\n");
+    debug_with_guard("[Begin] [Allocator Global Heap] Deallocate function\n");
     if (at == nullptr) return; // уже очищено?
 
     block_size_t meta_size = sizeof(allocator*) + sizeof(size_t);
@@ -102,7 +99,7 @@ void allocator_global_heap::deallocate(void *at)
 
     ::operator delete(block);
 
-    if (get_logger()) _logger->debug("[End] [Allocator Global Heap] Dellocate function\n");
+    debug_with_guard("[End] [Allocator Global Heap] Deallocate function\n");
 }
 
 inline logger *allocator_global_heap::get_logger() const
@@ -112,7 +109,7 @@ inline logger *allocator_global_heap::get_logger() const
 
 inline std::string allocator_global_heap::get_typename() const noexcept
 {
-    if (get_logger()) _logger->trace("[Begin] [Allocator Gloval Heap] Get_typename function\n");
-    if (get_logger()) _logger->trace("[End] [Allocator Gloval Heap] Get_typename function\n");
+    trace_with_guard("[Begin] [Allocator Gloval Heap] Get_typename function\n");
+    trace_with_guard("[End] [Allocator Gloval Heap] Get_typename function\n");
     return "allocator_global_heap";
 }
